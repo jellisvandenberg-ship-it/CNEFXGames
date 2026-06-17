ccde300307a177790b841f0c014981f10c8f4003
jellisvandenberg-ship-it <jellisvandenberg@gmail.com>
2026-06-17 14:59:03 +0200

    Add games array with TETRIS and SNAKE details

---
+++ games.js
@@ -0,0 +1,12 @@
+const games = [
+  {
+    name: "TETRIS",
+    file: "tetris.html",
+    desc: "Stack blocks"
+  },
+  {
+    name: "SNAKE",
+    file: "snake.html",
+    desc: "Classic snake"
+  }
+];
